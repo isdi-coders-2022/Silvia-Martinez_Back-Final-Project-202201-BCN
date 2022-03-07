0a404ec7874840322bd2f03f1576b3ef005d256e
Silvia Martinez <sil@silvia.com>
2022-03-07 19:28:09 +0100

    Added and connected DB

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ const debug = require("debug")("wallaplop:root");
 const chalk = require("chalk");
 const initServer = require("./server/initServer");
 const app = require("./server/index");
+const connectDatabase = require("./db");
 
 const port = process.env.PORT || 3089;
+const connectDB = process.env.MONGO_STRING;
 
 (async () => {
   try {
     await initServer(port, app);
+    await connectDatabase(connectDB);
   } catch (error) {
     debug(chalk.red(`Error:`, error.message));
   }

6ef4f92e986a925c020b79bf7ea70a452d13a5ea
Silvia Martinez <sil@silvia.com>
2022-03-08 15:46:34 +0100

    Added new product controller

---
+++ src/server/controllers/productControllers.js
@@ -0,0 +1,17 @@
+const debug = require("debug")("wallaplop:controllers:");
+const chalk = require("chalk");
+
+const Product = require("../../db/models/Product");
+
+const getAllProducts = async (req, res, next) => {
+  try {
+    const products = await Product.find();
+    res.status(200).json({ products });
+  } catch (error) {
+    debug(chalk.red("Error"));
+    error.status = 400;
+    next(error);
+  }
+};
+
+module.exports = getAllProducts;

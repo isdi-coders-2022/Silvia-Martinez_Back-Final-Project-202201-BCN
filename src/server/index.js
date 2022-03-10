2e62b1037f78dee4b5cc6d3b690d3b5b1183b1f4
Silvia Martinez <sil@silvia.com>
2022-03-10 14:09:10 +0100

    Added router at index

--- src/server/index.js
+++ src/server/index.js
@@ -3,15 +3,17 @@ const helmet = require("helmet");
 const morgan = require("morgan");
 const cors = require("cors");
 const productRouter = require("./routers/productRouter");
+const userRouter = require("./routers/userRouter");
 
 const app = express();
 
 app.use(morgan("dev"));
-app.use(helmet());
 app.use(cors());
+app.use(helmet());
 
 app.use(express.json());
 
 app.use("/products", productRouter);
+app.use("/user", userRouter);
 
 module.exports = app;

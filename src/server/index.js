f52ae8e78bb9cbc13d0bb3932439fd17bd5787f0
Silvia Martinez <sil@silvia.com>
2022-03-11 09:51:21 +0100

    Added new endpoint get User products

--- src/server/index.js
+++ src/server/index.js
@@ -3,7 +3,6 @@ const helmet = require("helmet");
 const morgan = require("morgan");
 const cors = require("cors");
 const productRouter = require("./routers/productRouter");
-const userRouter = require("./routers/userRouter");
 
 const app = express();
 
@@ -14,6 +13,5 @@ app.use(helmet());
 app.use(express.json());
 
 app.use("/products", productRouter);
-app.use("/user", userRouter);
 
 module.exports = app;
--- src/server/routers/productRouter.js
+++ src/server/routers/productRouter.js
@@ -1,8 +1,14 @@
 const express = require("express");
-const getAllProducts = require("../controllers/productControllers");
+
+const {
+  getAllProducts,
+  getUserProducts,
+} = require("../controllers/productControllers");
+const auth = require("../middlewares/auth");
 
 const router = express();
 
 router.get("/list", getAllProducts);
+router.get("/user", auth, getUserProducts);
 
 module.exports = router;

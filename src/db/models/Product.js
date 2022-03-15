715508f7274087eab7b0593cf48631846afb96a4
Silvia Martinez <sil@silvia.com>
2022-03-15 23:43:03 +0100

    Price to string

--- src/db/models/Product.js
+++ src/db/models/Product.js
@@ -1,7 +1,7 @@
 const { model, Schema } = require("mongoose");
 
 const ProductSchema = new Schema({
-  price: { type: Number, required: true },
+  price: { type: String, required: true },
   title: { type: String },
   description: { type: String, required: true },
   category: { type: String, required: true },
